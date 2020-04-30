af25879566d077110e7b488bcee23855569ed3cd
Fliss Tournant <flisstee@hotmail.co.uk>
2020-04-30 14:30:59 +0100

    Initial Commit

---
+++ Pace_Convert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pace_Convert
+//
+//  Created by Fliss Tournant on 30/04/2020.
+//  Copyright © 2020 Fliss Tournant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
