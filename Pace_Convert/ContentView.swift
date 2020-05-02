dce4f86f31ad423498317da8e9f7b709de503a09
Fliss Tournant <flisstee@hotmail.co.uk>
2020-05-02 22:43:49 +0100

    Update ContentView.swift
    initial commit

--- Pace_Convert/ContentView.swift
+++ Pace_Convert/ContentView.swift
@@ -7,12 +7,56 @@
 //
 
 import SwiftUI
+import UIKit
+
+//this currently works! calculates pace
 
 struct ContentView: View {
+    @State private var time = ""
+    @State private var distance = 12
+    
+    var pace: Double {
+        //this makes it so input can be in minutes, not secs
+        let times = Double(time) ?? 0
+        let distances = Double(distance) ?? 0// why doesn't this work?
+        let paces = Double((times * 60) / distances)
+        return Double(paces)
+        }
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    VStack {
+                        Text("Pace converter")
+                            .foregroundColor(.blue)
+                            .padding()
+            
+                        TextField("time in minutes", text: $time)
+                            .padding()
+                        
+                        TextField("time in minutes", text: $distance)
+                            .padding()
+                        
+                        Text("\(timeToString(Int(pace))) mins/km")
+                            .padding()
+                            }
+                        }
+                    }
+                }
+            }
+    func timeToString(_ input: Int) -> String {
+        let secs = input % 60
+        let mins = (input / 60) % 60
+        let hours  = input / 3600
+        if hours > 0 {
+            return String(format: "%d:%02d:%02d", hours, mins, secs)
+       } else {
+            return String(format: "%d:%02d", mins, secs)
+    }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
